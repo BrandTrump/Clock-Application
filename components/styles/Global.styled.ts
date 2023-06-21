0e4d7dca1c5f462acd000b20d34f370c16683ad5
Brandon Trump <brandontrump101@gmail.com>
2023-06-21 22:07:00 +1200

    style: added some global styles

---
+++ components/styles/Global.styled.ts
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    body {
+        background-color: rgb(240 240 240);
+        margin: 0;
+        padding: 0;
+    }
+`;
