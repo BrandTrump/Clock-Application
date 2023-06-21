2e49891ff600ce3dcd85099b9a4676e76d834b44
Brandon Trump <brandontrump101@gmail.com>
2023-06-21 23:12:42 +1200

    style: adjusted to match mobile template

--- components/styles/HomePage.styled.ts
+++ components/styles/HomePage.styled.ts
@@ -14,14 +14,51 @@ export const ClockInformation = styled.div`
   flex-direction: column;
   justify-content: space-between;
   height: 100%;
-  padding: 2em;
+  padding: 3em 2em;
 
   button {
     border: none;
     border-radius: 10em;
-    padding: 1.5em 5em;
+    padding: 1.5em;
     cursor: pointer;
     text-transform: uppercase;
     font-weight: 600;
+    width: 13em;
+  }
+`;
+
+export const QuoteContainer = styled.div`
+  h1 {
+    font-size: 1.1rem;
+  }
+`;
+
+export const TimeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4em;
+`;
+
+export const TimeInformation = styled.div`
+  h1 {
+    font-size: 6rem;
+    padding: 0;
+    margin: 0;
+
+    span {
+      font-size: 1rem;
+      font-weight: 300;
+    }
+  }
+
+  h2 {
+    margin: 0;
+    text-transform: uppercase;
+    font-weight: 300;
+
+    svg {
+      fill: white;
+      margin-right: 0.5em;
+    }
   }
 `;
