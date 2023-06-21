4bfb17ed1e35ede6f41c6974ebe9e11db5c5c64a
Brandon Trump <brandontrump101@gmail.com>
2023-06-21 22:08:20 +1200

    style: created a container to divide the content on the page

---
+++ components/styles/HomePage.styled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+  margin: 0;
+  padding: 2em;
+  background-color: salmon;
+  display: flex;
+  justify-content: space-around;
+`;
