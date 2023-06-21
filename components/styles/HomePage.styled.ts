fea36e237ad7c963bac96b22a141cda9961ed0a0
Brandon Trump <brandontrump101@gmail.com>
2023-06-21 22:42:21 +1200

    style: fixed page height & height of container

--- components/styles/HomePage.styled.ts
+++ components/styles/HomePage.styled.ts
@@ -2,8 +2,26 @@ import styled from "styled-components";
 
 export const MainContainer = styled.div`
   margin: 0;
-  padding: 2em;
+  padding: 0;
   background-color: salmon;
   display: flex;
-  justify-content: space-around;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+export const ClockInformation = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+  padding: 2em;
+
+  button {
+    border: none;
+    border-radius: 10em;
+    padding: 1.5em 5em;
+    cursor: pointer;
+    text-transform: uppercase;
+    font-weight: 600;
+  }
 `;
