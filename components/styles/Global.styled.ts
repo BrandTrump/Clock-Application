1501105dc6048afca38a72bad486454020f8c74c
Brandon Trump <brandontrump101@gmail.com>
2023-06-21 22:42:50 +1200

    style: changed text color to white

--- components/styles/Global.styled.ts
+++ components/styles/Global.styled.ts
@@ -3,7 +3,9 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyles = createGlobalStyle`
     body {
         background-color: rgb(240 240 240);
+        color: white;
         margin: 0;
         padding: 0;
+        height: 100%;
     }
 `;
