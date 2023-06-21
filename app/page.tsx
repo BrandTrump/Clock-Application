e46e25f3784a56105c7d05ebe27280924aaf2d1a
Brandon Trump <brandontrump101@gmail.com>
2023-06-21 22:09:26 +1200

    feat: imported the styled component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
-
+"use client";
+import { MainContainer } from "@/components/styles/HomePage.styled";
 
 export default function Home() {
   return (
-    <main>
-      Home Page
-    </main>
-  )
+    <MainContainer>
+      <div>
+        <div>
+          <div>
+            <p>&quot;This is an inspirational quote from...&quot;</p>
+          </div>
+          <h1>Ada Lovelace</h1>
+        </div>
+
+        <div>
+          <h2>Good Morning, It&apos;s currently</h2>
+          <h1>
+            11:37 <span>BST</span>
+          </h1>
+          <h2>In London, UK</h2>
+        </div>
+      </div>
+      <button>More</button>
+    </MainContainer>
+  );
 }
