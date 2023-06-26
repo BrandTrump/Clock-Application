55e85bccb3e70055ea3d1c0526fb41cb439a07f8
Brandon Trump <brandontrump101@gmail.com>
2023-06-27 00:48:07 +1200

    feat: component to display & convert the fetched data to the current time

---
+++ components/Clock.tsx
@@ -0,0 +1,43 @@
+import { getTime } from "@/helpers/getCurrentTime";
+import { useEffect, useState } from "react";
+
+type TimeInfo = {
+  datetime: number;
+};
+
+function Clock() {
+  const [time, setTime] = useState<TimeInfo>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getTime();
+        setTime(data);
+      } catch (error) {
+        console.error("Failed to fetch time:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const dateTime = new Date(time!.datetime);
+  const hours = dateTime.getUTCHours();
+  const minutes = dateTime.getUTCMinutes().toString().padStart(2, "0");
+  const time24HourFormat = `${hours}:${minutes}`;
+
+  return (
+    <>
+      {!time ? (
+        <p>Loading...</p>
+      ) : (
+        <h1>
+          {time24HourFormat}
+          <span>BST</span>
+        </h1>
+      )}
+    </>
+  );
+}
+
+export default Clock;
