1185708d6f55c53f9a678ce5ca1f9459cff774cd
Brandon Trump <brandontrump101@gmail.com>
2023-06-29 20:42:48 +1200

    style: improved responsiveness

--- components/styles/Clock.styled.ts
+++ components/styles/Clock.styled.ts
@@ -12,16 +12,19 @@ export const TimeInformation = styled.div`
     }
   }
 
+  svg {
+    fill: white;
+    width: 1em;
+    height: auto;
+  }
+
   h2 {
     margin: 0;
     text-transform: uppercase;
     font-weight: 300;
 
-    svg {
-      fill: white;
-      margin-right: 0.5em;
-      width: 0.85em;
-      height: auto;
+    span {
+      display: none;
     }
   }
 
@@ -36,6 +39,19 @@ export const TimeInformation = styled.div`
 
     h2 {
       font-size: 2.2rem;
+
+      span {
+        display: unset;
+      }
+    }
+
+    svg {
+      width: 1.4em;
     }
   }
+`;
+
+export const StyledGreeting = styled.div`
+  display: inline-flex;
+  gap: 1em;
 `;
