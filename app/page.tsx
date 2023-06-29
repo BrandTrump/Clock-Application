da5162da98ad9ac9ed97a1fbe93b979367e6baf8
Brandon Trump <brandontrump101@gmail.com>
2023-06-29 21:04:17 +1200

    feat: imported global styles

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import Clock from "@/components/Clock";
 import QuoteSection from "@/components/QuoteSection";
 import TimezoneSection from "@/components/TimezoneSection";
+import { GlobalStyles } from "@/components/styles/Global.styled";
 import {
   ClockInformation,
   MainContainer,
@@ -31,40 +32,43 @@ export default function Home() {
     setExpanded(!expanded);
   };
   return (
-    <PageContainer background={background.toString()}>
-      <MainContainer
-        expanded={expanded.toString()}
-        background={background.toString()}
-      >
-        <ClockInformation expanded={expanded.toString()}>
-          <QuoteContainer expanded={expanded.toString()}>
-            <QuoteSection />
-          </QuoteContainer>
+    <>
+      <GlobalStyles />
+      <PageContainer background={background.toString()}>
+        <MainContainer
+          expanded={expanded.toString()}
+          background={background.toString()}
+        >
+          <ClockInformation expanded={expanded.toString()}>
+            <QuoteContainer expanded={expanded.toString()}>
+              <QuoteSection />
+            </QuoteContainer>
 
-          <TimeContainer>
-            <Clock time={time} />
+            <TimeContainer>
+              <Clock time={time} />
 
-            <button onClick={handleClick}>
-              {!expanded ? "More" : "Less"}{" "}
-              <span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="1em"
-                  viewBox="0 0 448 512"
-                >
-                  <path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
-                </svg>
-              </span>
-            </button>
-          </TimeContainer>
-        </ClockInformation>
-      </MainContainer>
-      <TimezoneContainer
-        expanded={expanded.toString()}
-        background={background.toString()}
-      >
-        <TimezoneSection time={time} />
-      </TimezoneContainer>
-    </PageContainer>
+              <button onClick={handleClick}>
+                {!expanded ? "More" : "Less"}{" "}
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="1em"
+                    viewBox="0 0 448 512"
+                  >
+                    <path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+                  </svg>
+                </span>
+              </button>
+            </TimeContainer>
+          </ClockInformation>
+        </MainContainer>
+        <TimezoneContainer
+          expanded={expanded.toString()}
+          background={background.toString()}
+        >
+          <TimezoneSection time={time} />
+        </TimezoneContainer>
+      </PageContainer>
+    </>
   );
 }
