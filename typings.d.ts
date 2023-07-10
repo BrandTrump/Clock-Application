83270c949dfc4a475978b8c7e0610279666a77eb
Brandon Trump <brandontrump101@gmail.com>
2023-07-10 16:14:27 +1200

    feat: updated types for new set of data

--- typings.d.ts
+++ typings.d.ts
@@ -1,8 +1,37 @@
-interface TimeInfo {
-  utc_datetime: number;
-  abbreviation: string;
-  timezone: string;
-  day_of_week: number;
-  day_of_year: number;
-  week_number: number;
+interface Located {
+  continent: {
+    code: string;
+    name: string;
+  };
+  country: {
+    calling_codes: string[];
+    currencies: {
+      name: string;
+    }[];
+    languages: {
+      name: string;
+    }[];
+    name: string;
+    timezones: string[];
+  };
+  city: {
+    name: string;
+  };
+}
+
+interface Timezone {
+  id: string;
+  current_time: string;
+  code: string;
+  is_daylight_saving: boolean;
+  gmt_offset: number;
+}
+
+interface Data {
+  timezone: Timezone;
+  location: Located;
+}
+
+interface FetchedData {
+  data: Data;
 }
